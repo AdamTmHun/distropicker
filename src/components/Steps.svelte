7820f4b7034fdd9fb602cdb68275dc6d8e382b55
AdamTmHun <estoka07@gmail.com>
2022-06-27 00:19:23 +0200

    Questions: Part 2

--- src/components/Steps.svelte
+++ src/components/Steps.svelte
@@ -4,13 +4,13 @@
     const passed = "step step-primary duration-300"
 </script>
 
-<ul class="steps hidden lg:grid mt-4 durat">
+<ul class="steps hidden lg:grid mt-4">
         <li class={step >= 1 ? passed : notPassed}>Usecase</li>
         <li class={step >= 2 ? passed : notPassed}>Knowledge of Linux</li>
         <li class={step >= 3 ? passed : notPassed}>Installation Tweaks</li>
-        <li class={step >= 4 ? passed : notPassed}>Hardware</li>
-        <li class={step >= 5 ? passed : notPassed}>Community</li>
-        <li class={step >= 6 ? passed : notPassed}>User Experience</li>
+        <li class={step >= 4 ? passed : notPassed}>Architecture</li>
+        <li class={step >= 5 ? passed : notPassed}>Hardware</li>
+        <li class={step >= 6 ? passed : notPassed}>Community</li>
         <li class={step >= 7 ? passed : notPassed}>Costs</li>
         <li class={step >= 8 ? passed : notPassed}>Distro Scope</li>
         <li class={step >= 9 ? passed : notPassed}>Applications</li>
