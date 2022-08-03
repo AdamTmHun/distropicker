a68ad7e4b5c82aa5a049f85e73a1007bb57dc14e
AdamTmHun <estoka07@outlook.com>
2022-08-03 22:58:34 +0200

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,7 @@ const config = {
 	})],
 
 	kit: {
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
